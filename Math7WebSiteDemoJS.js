984ec693554e27cb03a1699f9d31c4833d129be8
rogue-captain <mr.suleyman.erkan@gmail.com>
2024-08-17 19:46:07 -0400

    modded some html, JS to remove debugging features

--- Math7WebSiteDemoJS.js
+++ Math7WebSiteDemoJS.js
@@ -1,6 +1,5 @@
 
-  
-var answer 
+  var answer 
 
 function generateProblem(difficulty) {
 
@@ -12,11 +11,11 @@ var problemText = `${operand1} ${operator} ${operand2}`;
 answer = eval(problemText);  
 
 //DEBUGGING LINE ----------------------------------------------------------------------------------------->
-  alert("generateProblem() executed. answer = " + answer)
+//  alert("generateProblem() executed. answer = " + answer)
 
 //DEBUGGING LINE ----------------------------------------------------------------------------------------->
   var resultElement = document.getElementById('result');
-resultElement.innerText = 'Answer = ' + answer;
+// resultElement.innerText = 'Answer = ' + answer;
  //DEBUGGING LINE ----------------------------------------------------------------------------------------->
 
 return {
@@ -33,7 +32,7 @@ var userAnswer = parseInt(document.getElementById('answer').value);
 if (!isNaN(userAnswer)) {
 
   //DEBUGGING LINE ----------------------------------------------------------------------------------------->
-  alert('checkAnswer() called: answer = ' + answer + ', userAnswer = ' + userAnswer);
+  //alert('checkAnswer() called: answer = ' + answer + ', userAnswer = ' + userAnswer);
   //DEBUGGING LINE ----------------------------------------------------------------------------------------->
   
   if (answer === userAnswer) {
